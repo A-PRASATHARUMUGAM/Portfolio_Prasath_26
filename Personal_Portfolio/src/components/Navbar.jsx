32ef95eec8565b5e6c4c863d1125a4c8c254c605
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-03-07 17:36:08 +0530

    Implement the again structure about the navigation design

--- Personal_Portfolio/src/components/Navbar.jsx
+++ Personal_Portfolio/src/components/Navbar.jsx
@@ -7,15 +7,11 @@ const Navbar= ()=>{
         <>
             <div className="nav-container  border-white flex justify-center  items-center p-4  ">
  
-                <div className="navbar  font-line border-2 rounded-4xl  flex py-2 px-8 justify-center cursor-pointer text-white ">
-                        <a href="" className="mx-4  hover:font-semibold duration-300 ease">
-                             <span>
-                                
-                            </span> 
-                            <p>  Home</p>
-                          
-                           
-                            
+                <div className="navbar  font-line border-2 rounded-4xl   flex justify-center cursor-pointer text-white ">
+
+
+                        <a href="" className="mx-4 hover:border  hover:p-1 flex justify-center items-center hover:font-semibold duration-300 ease">
+                              Home 
                         </a>
                         <a href="#" className="mx-4 hover:font-semibold duration-300 ease">About</a> 
                         <a href="#" className="mx-4 hover:font-semibold duration-300 ease">Service</a> 
