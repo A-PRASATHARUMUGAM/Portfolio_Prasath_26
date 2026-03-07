7ce727a1171c8922f5eb134350e34e544347079b
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-03-07 18:01:44 +0530

    Top Navigation bar has been completed with perfect pixel

--- Personal_Portfolio/src/components/Navbar.jsx
+++ Personal_Portfolio/src/components/Navbar.jsx
@@ -7,18 +7,30 @@ const Navbar= ()=>{
         <>
             <div className="nav-container  border-white flex justify-center  items-center p-4  ">
  
-                <div className="navbar  font-line border-2 rounded-4xl   flex justify-center cursor-pointer text-white ">
+                     <div className="navbar flex items-center px-6 py-3 
+                         bg-white/10 backdrop-blur-xl 
+                        border border-white/20 
+                        rounded-full shadow-lg  font-line  cursor-pointer text-white ">
 
-
-                        <a href="" className="mx-4 hover:border  hover:p-1 flex justify-center items-center hover:font-semibold duration-300 ease">
+ 
+                        <a href="#" className="mx-4  hover:border hover:shadow-lg  hover:bg-white/10  hover:backdrop-blur-xl  rounded-full hover:border-white/40   hover:px-4 hover:py-1 flex justify-center items-center hover:font-semibold duration-300 ease">
                               Home 
                         </a>
-                        <a href="#" className="mx-4 hover:font-semibold duration-300 ease">About</a> 
-                        <a href="#" className="mx-4 hover:font-semibold duration-300 ease">Service</a> 
-                        <a href="#" className="mx-4 hover:font-semibold duration-300 ease">Projects</a> 
-                        <a href="#" className="mx-4 hover:font-semibold duration-300 ease">Contact</a>
+                        <a href="#" className="mx-4 hover:border hover:shadow-lg  hover:bg-white/10 hover:backdrop-blur-xl  rounded-full hover:border-white/40    hover:px-4 hover:py-1 flex justify-center items-center hover:font-semibold duration-300 ease">
+                              About 
+                        </a>
+                        <a href="#" className="mx-4    hover:border hover:shadow-lg  hover:bg-white/10 hover:backdrop-blur-xl  rounded-full hover:border-white/40   hover:px-4 hover:py-1 flex justify-center items-center hover:font-semibold duration-300 ease">
+                              Service 
+                        </a>
+                        <a href="#" className="mx-4    hover:border hover:shadow-lg  hover:bg-white/10 hover:backdrop-blur-xl  rounded-full hover:border-white/40  hover:px-4 hover:py-1 flex justify-center items-center hover:font-semibold duration-300 ease">
+                              Project 
+                        </a>
+                        <a href="#" className="mx-4 hover:border hover:shadow-lg  hover:bg-white/10 hover:backdrop-blur-xl  rounded-full hover:border-white/40   hover:px-4 hover:py-1 flex justify-center items-center hover:font-semibold duration-300 ease">
+                              Contact 
+                        </a>
                 </div>
             </div>
+
         </>
     )
 
