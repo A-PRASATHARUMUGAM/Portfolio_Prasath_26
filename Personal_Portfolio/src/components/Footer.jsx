d18d3f7598b535be8abfffda6268c9a08e6f4ba4
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-03-07 16:27:26 +0530

    Remove the unwanted code

--- Personal_Portfolio/src/components/Footer.jsx
+++ Personal_Portfolio/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer=() => {
     return (
         <>
         <div>
-                     <h1>Iam trying to improve my life its very hard to consistency</h1>
+                 <>Footer </>
         </div>
         
         </>
--- Personal_Portfolio/src/components/Navbar.jsx
+++ Personal_Portfolio/src/components/Navbar.jsx
@@ -5,16 +5,10 @@ const Navbar= ()=>{
 
     return (
         <>
-            <div className="nav-container border-1 border-white flex justify-end items-center ">
+            <div className="nav-container border border-white flex justify-end items-center ">
 
-                <div className="navbar border-1 border-yellow-300  flex p-1 justify-evenly  text-white ">
+                <div className="navbar border border-yellow-300  flex p-1 justify-evenly  text-white ">
                         <a href="" className="mx-4">Home</a>
-                        <a href="" className="mx-4">About</a> 
-                        <a href="" className="mx-4">Service</a> 
-                        <a href="" className="mx-4">Projects</a> 
-                        <a href="" className="mx-4">Contact</a>
-
-a href="" className="mx-4">Home</a>
                         <a href="" className="mx-4">About</a> 
                         <a href="" className="mx-4">Service</a> 
                         <a href="" className="mx-4">Projects</a> 
