707ae663d52b999d1c5cf541d261a0c5a0ededda
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-03-08 23:57:57 +0530

    add come changes

--- Personal_Portfolio/src/components/Home.jsx
+++ Personal_Portfolio/src/components/Home.jsx
@@ -5,26 +5,24 @@ const Home = () =>{
 
 return (
 <>
-<div className=" flex  border ">
+<div className=" flex  ">
 
-       <div className="border-white mt-20 h-140 flex justify-start items-start p-10 border">
+       <div className="mt-20 h-140 flex justify-start items-start p-10  w-full">
 
  
-            <div className="profile-container flex   bg-white/5 backdrop-blur-xl rounded-full p-5   border border-white/20   ">
+            <div className="profile-container flex bg-white/5 backdrop-blur-xl rounded-full p-5   border border-white/20   ">
 
-                <h1 className="text-3xl font-extrabold text-white p-2 ">
+                <h1 className="text-2xl font-extrabold text-white p-2 ">
                     Hi, I'm Prasath
                     </h1>
-                    <p className="text-3xl font-semibold p-2 ">
+                    <p className="text-2xl font-semibold p-2 ">
                     Full Stack Web Developer
                     </p>
 
 
             </div>
 
-            <div className="chat-container">
-                
-            </div>
+        
 
           
        </div>
