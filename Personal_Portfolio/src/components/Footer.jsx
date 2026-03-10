a34421aa1a0d2492f7c07c45dbfca08ea30e9e9e
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-03-10 22:41:11 +0530

    create the paragraph in the footer

--- Personal_Portfolio/src/components/Footer.jsx
+++ Personal_Portfolio/src/components/Footer.jsx
@@ -7,6 +7,8 @@ const Footer=() => {
         <>
         <div>
                  <>Footer </>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quibusdam. Necessitatibus neque dolore possimus repudiandae dignissimos ipsum similique molestiae magni voluptate. Facilis, quisquam! Ullam, eum enim iste hic nihil eos.</p>
         </div>
         
         </>
