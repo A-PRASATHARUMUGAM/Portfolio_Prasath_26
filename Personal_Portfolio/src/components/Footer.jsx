bec0b1d0fa8b9a21dcd0acf0e5f0b44261803a53
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-03-10 22:42:35 +0530

    it is working fine

--- Personal_Portfolio/src/components/Footer.jsx
+++ Personal_Portfolio/src/components/Footer.jsx
@@ -8,7 +8,13 @@ const Footer=() => {
         <div>
                  <>Footer </>
 
-             
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quibusdam. Necessitatibus neque dolore possimus repudiandae dignissimos ipsum similique molestiae magni voluptate. Facilis, quisquam! Ullam, eum enim iste hic nihil eos.</p>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quibusdam. Necessitatibus neque dolore possimus repudiandae dignissimos ipsum similique molestiae magni voluptate. Facilis, quisquam! Ullam, eum enim iste hic nihil eos.</p>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quibusdam. Necessitatibus neque dolore possimus repudiandae dignissimos ipsum similique molestiae magni voluptate. Facilis, quisquam! Ullam, eum enim iste hic nihil eos.</p>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quibusdam. Necessitatibus neque dolore possimus repudiandae dignissimos ipsum similique molestiae magni voluptate. Facilis, quisquam! Ullam, eum enim iste hic nihil eos.</p>
         </div>
         
         </>
