7951f7ab3cb11fcbbfda9bdcce1d594975940d1b
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-03-11 20:58:56 +0530

    Deleted unwanted code

--- Personal_Portfolio/src/components/Footer.jsx
+++ Personal_Portfolio/src/components/Footer.jsx
@@ -7,14 +7,6 @@ const Footer=() => {
         <>
         <div>
                  <>Footer </>
-
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quibusdam. Necessitatibus neque dolore possimus repudiandae dignissimos ipsum similique molestiae magni voluptate. Facilis, quisquam! Ullam, eum enim iste hic nihil eos.</p>
-
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quibusdam. Necessitatibus neque dolore possimus repudiandae dignissimos ipsum similique molestiae magni voluptate. Facilis, quisquam! Ullam, eum enim iste hic nihil eos.</p>
-
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quibusdam. Necessitatibus neque dolore possimus repudiandae dignissimos ipsum similique molestiae magni voluptate. Facilis, quisquam! Ullam, eum enim iste hic nihil eos.</p>
-
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quibusdam. Necessitatibus neque dolore possimus repudiandae dignissimos ipsum similique molestiae magni voluptate. Facilis, quisquam! Ullam, eum enim iste hic nihil eos.</p>
         </div>
         
         </>
